5b30c9227f318513bf4486c85168af98961e57e6
Alexander Hramov <alexander.hramov@aspose.com>
2022-09-15 16:05:51 +0300

    MERGERAPP-173: Full translation for all the templates.

---
+++ templates/utils/translate_ALL_landings.bat
@@ -0,0 +1,188 @@
+Translation\Bin\net6.0\Res.Translator.exe -r ..\data\combine_net.json -d ru --overwrite
+Translation\Bin\net6.0\Res.Translator.exe -r ..\data\combine_java.json -d ru --overwrite
+
+Translation\Bin\net6.0\Res.Translator.exe -r ..\data\combine_net.json -d ja --overwrite
+Translation\Bin\net6.0\Res.Translator.exe -r ..\data\combine_java.json -d ja --overwrite
+
+Translation\Bin\net6.0\Res.Translator.exe -r ..\data\combine_net.json -d de --overwrite
+Translation\Bin\net6.0\Res.Translator.exe -r ..\data\combine_java.json -d de --overwrite
+
+Translation\Bin\net6.0\Res.Translator.exe -r ..\data\combine_net.json -d es --overwrite
+Translation\Bin\net6.0\Res.Translator.exe -r ..\data\combine_java.json -d es --overwrite
+
+Translation\Bin\net6.0\Res.Translator.exe -r ..\data\combine_net.json -d fr --overwrite
+Translation\Bin\net6.0\Res.Translator.exe -r ..\data\combine_java.json -d fr --overwrite
+
+Translation\Bin\net6.0\Res.Translator.exe -r ..\data\combine_net.json -d ko --overwrite
+Translation\Bin\net6.0\Res.Translator.exe -r ..\data\combine_java.json -d ko --overwrite
+
+Translation\Bin\net6.0\Res.Translator.exe -r ..\data\combine_net.json -d zh-cn --overwrite
+Translation\Bin\net6.0\Res.Translator.exe -r ..\data\combine_java.json -d zh-cn --overwrite
+
+Translation\Bin\net6.0\Res.Translator.exe -r ..\data\extract_net.json -d ru --overwrite
+Translation\Bin\net6.0\Res.Translator.exe -r ..\data\extract_java.json -d ru --overwrite
+
+Translation\Bin\net6.0\Res.Translator.exe -r ..\data\extract_net.json -d ja --overwrite
+Translation\Bin\net6.0\Res.Translator.exe -r ..\data\extract_java.json -d ja --overwrite
+
+Translation\Bin\net6.0\Res.Translator.exe -r ..\data\extract_net.json -d de --overwrite
+Translation\Bin\net6.0\Res.Translator.exe -r ..\data\extract_java.json -d de --overwrite
+
+Translation\Bin\net6.0\Res.Translator.exe -r ..\data\extract_net.json -d es --overwrite
+Translation\Bin\net6.0\Res.Translator.exe -r ..\data\extract_java.json -d es --overwrite
+
+Translation\Bin\net6.0\Res.Translator.exe -r ..\data\extract_net.json -d fr --overwrite
+Translation\Bin\net6.0\Res.Translator.exe -r ..\data\extract_java.json -d fr --overwrite
+
+Translation\Bin\net6.0\Res.Translator.exe -r ..\data\extract_net.json -d ko --overwrite
+Translation\Bin\net6.0\Res.Translator.exe -r ..\data\extract_java.json -d ko --overwrite
+
+Translation\Bin\net6.0\Res.Translator.exe -r ..\data\extract_net.json -d zh-cn --overwrite
+Translation\Bin\net6.0\Res.Translator.exe -r ..\data\extract_java.json -d zh-cn --overwrite
+
+Translation\Bin\net6.0\Res.Translator.exe -r ..\data\join_net.json -d ru --overwrite
+Translation\Bin\net6.0\Res.Translator.exe -r ..\data\join_java.json -d ru --overwrite
+
+Translation\Bin\net6.0\Res.Translator.exe -r ..\data\join_net.json -d ja --overwrite
+Translation\Bin\net6.0\Res.Translator.exe -r ..\data\join_java.json -d ja --overwrite
+
+Translation\Bin\net6.0\Res.Translator.exe -r ..\data\join_net.json -d de --overwrite
+Translation\Bin\net6.0\Res.Translator.exe -r ..\data\join_java.json -d de --overwrite
+
+Translation\Bin\net6.0\Res.Translator.exe -r ..\data\join_net.json -d es --overwrite
+Translation\Bin\net6.0\Res.Translator.exe -r ..\data\join_java.json -d es --overwrite
+
+Translation\Bin\net6.0\Res.Translator.exe -r ..\data\join_net.json -d fr --overwrite
+Translation\Bin\net6.0\Res.Translator.exe -r ..\data\join_java.json -d fr --overwrite
+
+Translation\Bin\net6.0\Res.Translator.exe -r ..\data\join_net.json -d ko --overwrite
+Translation\Bin\net6.0\Res.Translator.exe -r ..\data\join_java.json -d ko --overwrite
+
+Translation\Bin\net6.0\Res.Translator.exe -r ..\data\join_net.json -d zh-cn --overwrite
+Translation\Bin\net6.0\Res.Translator.exe -r ..\data\join_java.json -d zh-cn --overwrite
+
+Translation\Bin\net6.0\Res.Translator.exe -r ..\data\merge_net.json -d ru --overwrite
+Translation\Bin\net6.0\Res.Translator.exe -r ..\data\merge_java.json -d ru --overwrite
+
+Translation\Bin\net6.0\Res.Translator.exe -r ..\data\merge_net.json -d ja --overwrite
+Translation\Bin\net6.0\Res.Translator.exe -r ..\data\merge_java.json -d ja --overwrite
+
+Translation\Bin\net6.0\Res.Translator.exe -r ..\data\merge_net.json -d de --overwrite
+Translation\Bin\net6.0\Res.Translator.exe -r ..\data\merge_java.json -d de --overwrite
+
+Translation\Bin\net6.0\Res.Translator.exe -r ..\data\merge_net.json -d es --overwrite
+Translation\Bin\net6.0\Res.Translator.exe -r ..\data\merge_java.json -d es --overwrite
+
+Translation\Bin\net6.0\Res.Translator.exe -r ..\data\merge_net.json -d fr --overwrite
+Translation\Bin\net6.0\Res.Translator.exe -r ..\data\merge_java.json -d fr --overwrite
+
+Translation\Bin\net6.0\Res.Translator.exe -r ..\data\merge_net.json -d ko --overwrite
+Translation\Bin\net6.0\Res.Translator.exe -r ..\data\merge_java.json -d ko --overwrite
+
+Translation\Bin\net6.0\Res.Translator.exe -r ..\data\merge_net.json -d zh-cn --overwrite
+Translation\Bin\net6.0\Res.Translator.exe -r ..\data\merge_java.json -d zh-cn --overwrite
+
+Translation\Bin\net6.0\Res.Translator.exe -r ..\data\move_net.json -d ru --overwrite
+Translation\Bin\net6.0\Res.Translator.exe -r ..\data\move_java.json -d ru --overwrite
+
+Translation\Bin\net6.0\Res.Translator.exe -r ..\data\move_net.json -d ja --overwrite
+Translation\Bin\net6.0\Res.Translator.exe -r ..\data\move_java.json -d ja --overwrite
+
+Translation\Bin\net6.0\Res.Translator.exe -r ..\data\move_net.json -d de --overwrite
+Translation\Bin\net6.0\Res.Translator.exe -r ..\data\move_java.json -d de --overwrite
+
+Translation\Bin\net6.0\Res.Translator.exe -r ..\data\move_net.json -d es --overwrite
+Translation\Bin\net6.0\Res.Translator.exe -r ..\data\move_java.json -d es --overwrite
+
+Translation\Bin\net6.0\Res.Translator.exe -r ..\data\move_net.json -d fr --overwrite
+Translation\Bin\net6.0\Res.Translator.exe -r ..\data\move_java.json -d fr --overwrite
+
+Translation\Bin\net6.0\Res.Translator.exe -r ..\data\move_net.json -d ko --overwrite
+Translation\Bin\net6.0\Res.Translator.exe -r ..\data\move_java.json -d ko --overwrite
+
+Translation\Bin\net6.0\Res.Translator.exe -r ..\data\move_net.json -d zh-cn --overwrite
+Translation\Bin\net6.0\Res.Translator.exe -r ..\data\move_java.json -d zh-cn --overwrite
+
+Translation\Bin\net6.0\Res.Translator.exe -r ..\data\remove_net.json -d ru --overwrite
+Translation\Bin\net6.0\Res.Translator.exe -r ..\data\remove_java.json -d ru --overwrite
+
+Translation\Bin\net6.0\Res.Translator.exe -r ..\data\remove_net.json -d ja --overwrite
+Translation\Bin\net6.0\Res.Translator.exe -r ..\data\remove_java.json -d ja --overwrite
+
+Translation\Bin\net6.0\Res.Translator.exe -r ..\data\remove_net.json -d de --overwrite
+Translation\Bin\net6.0\Res.Translator.exe -r ..\data\remove_java.json -d de --overwrite
+
+Translation\Bin\net6.0\Res.Translator.exe -r ..\data\remove_net.json -d es --overwrite
+Translation\Bin\net6.0\Res.Translator.exe -r ..\data\remove_java.json -d es --overwrite
+
+Translation\Bin\net6.0\Res.Translator.exe -r ..\data\remove_net.json -d fr --overwrite
+Translation\Bin\net6.0\Res.Translator.exe -r ..\data\remove_java.json -d fr --overwrite
+
+Translation\Bin\net6.0\Res.Translator.exe -r ..\data\remove_net.json -d ko --overwrite
+Translation\Bin\net6.0\Res.Translator.exe -r ..\data\remove_java.json -d ko --overwrite
+
+Translation\Bin\net6.0\Res.Translator.exe -r ..\data\remove_net.json -d zh-cn --overwrite
+Translation\Bin\net6.0\Res.Translator.exe -r ..\data\remove_java.json -d zh-cn --overwrite
+
+Translation\Bin\net6.0\Res.Translator.exe -r ..\data\rotate_net.json -d ru --overwrite
+Translation\Bin\net6.0\Res.Translator.exe -r ..\data\rotate_java.json -d ru --overwrite
+
+Translation\Bin\net6.0\Res.Translator.exe -r ..\data\rotate_net.json -d ja --overwrite
+Translation\Bin\net6.0\Res.Translator.exe -r ..\data\rotate_java.json -d ja --overwrite
+
+Translation\Bin\net6.0\Res.Translator.exe -r ..\data\rotate_net.json -d de --overwrite
+Translation\Bin\net6.0\Res.Translator.exe -r ..\data\rotate_java.json -d de --overwrite
+
+Translation\Bin\net6.0\Res.Translator.exe -r ..\data\rotate_net.json -d es --overwrite
+Translation\Bin\net6.0\Res.Translator.exe -r ..\data\rotate_java.json -d es --overwrite
+
+Translation\Bin\net6.0\Res.Translator.exe -r ..\data\rotate_net.json -d fr --overwrite
+Translation\Bin\net6.0\Res.Translator.exe -r ..\data\rotate_java.json -d fr --overwrite
+
+Translation\Bin\net6.0\Res.Translator.exe -r ..\data\rotate_net.json -d ko --overwrite
+Translation\Bin\net6.0\Res.Translator.exe -r ..\data\rotate_java.json -d ko --overwrite
+
+Translation\Bin\net6.0\Res.Translator.exe -r ..\data\rotate_net.json -d zh-cn --overwrite
+Translation\Bin\net6.0\Res.Translator.exe -r ..\data\rotate_java.json -d zh-cn --overwrite
+
+Translation\Bin\net6.0\Res.Translator.exe -r ..\data\split_net.json -d ru --overwrite
+Translation\Bin\net6.0\Res.Translator.exe -r ..\data\split_java.json -d ru --overwrite
+
+Translation\Bin\net6.0\Res.Translator.exe -r ..\data\split_net.json -d ja --overwrite
+Translation\Bin\net6.0\Res.Translator.exe -r ..\data\split_java.json -d ja --overwrite
+
+Translation\Bin\net6.0\Res.Translator.exe -r ..\data\split_net.json -d de --overwrite
+Translation\Bin\net6.0\Res.Translator.exe -r ..\data\split_java.json -d de --overwrite
+
+Translation\Bin\net6.0\Res.Translator.exe -r ..\data\split_net.json -d es --overwrite
+Translation\Bin\net6.0\Res.Translator.exe -r ..\data\split_java.json -d es --overwrite
+
+Translation\Bin\net6.0\Res.Translator.exe -r ..\data\split_net.json -d fr --overwrite
+Translation\Bin\net6.0\Res.Translator.exe -r ..\data\split_java.json -d fr --overwrite
+
+Translation\Bin\net6.0\Res.Translator.exe -r ..\data\split_net.json -d ko --overwrite
+Translation\Bin\net6.0\Res.Translator.exe -r ..\data\split_java.json -d ko --overwrite
+
+Translation\Bin\net6.0\Res.Translator.exe -r ..\data\split_net.json -d zh-cn --overwrite
+Translation\Bin\net6.0\Res.Translator.exe -r ..\data\split_java.json -d zh-cn --overwrite
+
+Translation\Bin\net6.0\Res.Translator.exe -r ..\data\swap_net.json -d ru --overwrite
+Translation\Bin\net6.0\Res.Translator.exe -r ..\data\swap_java.json -d ru --overwrite
+
+Translation\Bin\net6.0\Res.Translator.exe -r ..\data\swap_net.json -d ja --overwrite
+Translation\Bin\net6.0\Res.Translator.exe -r ..\data\swap_java.json -d ja --overwrite
+
+Translation\Bin\net6.0\Res.Translator.exe -r ..\data\swap_net.json -d de --overwrite
+Translation\Bin\net6.0\Res.Translator.exe -r ..\data\swap_java.json -d de --overwrite
+
+Translation\Bin\net6.0\Res.Translator.exe -r ..\data\swap_net.json -d es --overwrite
+Translation\Bin\net6.0\Res.Translator.exe -r ..\data\swap_java.json -d es --overwrite
+
+Translation\Bin\net6.0\Res.Translator.exe -r ..\data\swap_net.json -d fr --overwrite
+Translation\Bin\net6.0\Res.Translator.exe -r ..\data\swap_java.json -d fr --overwrite
+
+Translation\Bin\net6.0\Res.Translator.exe -r ..\data\swap_net.json -d ko --overwrite
+Translation\Bin\net6.0\Res.Translator.exe -r ..\data\swap_java.json -d ko --overwrite
+
+Translation\Bin\net6.0\Res.Translator.exe -r ..\data\swap_net.json -d zh-cn --overwrite
+Translation\Bin\net6.0\Res.Translator.exe -r ..\data\swap_java.json -d zh-cn --overwrite
